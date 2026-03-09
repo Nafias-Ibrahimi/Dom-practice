76617bfa937b385de7834521a2747d3c93319c92
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-09 22:47:41 +0430

    use forEach

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
-const title=document.getElementById('title');
-const items=document.getElementsByClassName('text');
-const el=document.getElementsByTagName('p');
-const els=document.querySelector('.text');
-// els.forEach(e=>console.log(e));
+// const title=document.getElementById('title');
+// const items=document.getElementsByClassName('text');
+// const el=document.getElementsByTagName('p');
+// const els=document.querySelector('.text');
+// // els.forEach(e=>console.log(e));
 
-// تغییر محتوا (Changing Content)
-// innerText
+// // تغییر محتوا (Changing Content)
+// // innerText
 
-// element.innertext='new Text';
+// // element.innertext='new Text';
 
 
-// innerHTML innerHTML تمام HTML داخل یک عنصر را می‌خواند یا تغییر می‌دهد.
- const box=document.getElementsByClassName('box');
- box.innerHtml='<b>Hello</b>'
+// // innerHTML innerHTML تمام HTML داخل یک عنصر را می‌خواند یا تغییر می‌دهد.
+//  const box=document.getElementsByClassName('box');
+//  box.innerHtml='<b>Hello</b>'
 
-// innerText
-// innerText فقط متن ساده را می‌خواند یا تغییر می‌دهد.
+// // innerText
+// // innerText فقط متن ساده را می‌خواند یا تغییر می‌دهد.
 
-box.innertext='<b>Hello World</b>';
-console.log(box);
+// box.innertext='<b>Hello World</b>';
+// console.log(box);
 
-// textContent
-const item=document.getElementsByClassName('text');
+// // textContent
+// const item=document.getElementsByClassName('text');
 
-console.log(item);
-for(let i=0; i<item.length; i++){
-    console.log(item[i]);
-}
+// console.log(item);
+// for(let i=0; i<item.length; i++){
+//     console.log(item[i]);
+// }
 
 // getElementsByTagName
 
-const paragraphs=document.getElementsByTagName('p');
+// const paragraphs=document.getElementsByTagName('p');
 
-const element=document.querySelector('.text');
+// const element=document.querySelector('.text');
 
 // document.querySelector("#title")
 // document.querySelector(".text")
 // document.querySelector("p")
-const elements=document.querySelectorAll('.text');
-elements.forEach(el=>{
-    console.log(el);
+// const elements=document.querySelectorAll('.text');
+// elements.forEach(el=>{
+//     console.log(el);
+// })
+
+// document.getElementById()
+
+// document.getElementsByClassName()
+
+// document.getElementsByTagName()
+
+// document.querySelector()
+
+// document.querySelectorAll()
+
+
+// today's use the modern seletors
+// querySelector
+// querySelectorAll
+const list=document.createElement('ul');
+list.id='list';
+document.body.appendChild(list);
+
+const data=['A','B','C'];
+
+data.forEach(text =>{
+    const li=document.createElement('li');
+    li.className='item';
+    li.innertext=text;
+    list.appendChild(li);
+});
+const item1=document.querySelectorAll('.item');
+item1.forEach(item =>{
+    item.style.color='red';
 })
+
+
