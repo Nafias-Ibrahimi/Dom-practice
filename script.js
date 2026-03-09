17bf74bdd1ee5a681954817fbce65eed10515f39
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-09 22:26:31 +0430

    getElementsByTagName

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ const title=document.getElementById('title');
 const items=document.getElementsByClassName('text');
 const el=document.getElementsByTagName('p');
 const els=document.querySelector('.text');
-els.forEach(e=>console.log(e));
+// els.forEach(e=>console.log(e));
 
 // تغییر محتوا (Changing Content)
 // innerText
 
-element.innertext='new Text';
+// element.innertext='new Text';
 
 
 // innerHTML innerHTML تمام HTML داخل یک عنصر را می‌خواند یا تغییر می‌دهد.
  const box=document.getElementsByClassName('box');
  box.innerHtml='<b>Hello</b>'
+
+// innerText
+// innerText فقط متن ساده را می‌خواند یا تغییر می‌دهد.
+
+box.innertext='<b>Hello World</b>';
+console.log(box);
+
+// textContent
+const item=document.getElementsByClassName('text');
+
+console.log(item);
+for(let i=0; i<item.length; i++){
+    console.log(item[i]);
+}
+
+// getElementsByTagName
