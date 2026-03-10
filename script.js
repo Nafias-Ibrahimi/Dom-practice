10e97ebfc573d203fcdf71847a25278f0c1a9a40
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-10 10:58:34 +0430

    used selector

--- script.js
+++ script.js
@@ -56,68 +56,98 @@
 // today's use the modern seletors
 // querySelector
 // querySelectorAll
-const list=document.createElement('ul');
-list.id='list';
-document.body.appendChild(list);
+// const list=document.createElement('ul');
+// list.id='list';
+// document.body.appendChild(list);
 
-const data=['A','B','C'];
+// const data=['A','B','C'];
 
-data.forEach(text =>{
-    const li=document.createElement('li');
-    li.className='item';
-    li.innertext=text;
-    list.appendChild(li);
-});
-const item1=document.querySelectorAll('.item');
-item1.forEach(item =>{
-    item.style.color='red';
-})
+// data.forEach(text =>{
+//     const li=document.createElement('li');
+//     li.className='item';
+//     li.innertext=text;
+//     list.appendChild(li);
+// });
+// const item1=document.querySelectorAll('.item');
+// item1.forEach(item =>{
+//     item.style.color='red';
+// })
 
-const newDiv=document.createElement('div');
-newDiv.textContent='hello from js';
-document.body.appendChild(newDiv);
+// const newDiv=document.createElement('div');
+// newDiv.textContent='hello from js';
+// document.body.appendChild(newDiv);
 
-document.body.prepend(newDiv);
-const parent = document.querySelector("#list");
-const ref = document.querySelector("#item2");
-const newItem = document.createElement("li");
-newItem.textContent = "Inserted Item";
-parent.insertBefore(newItem, ref);
-
-
-// parent.removerChild(ref);
-// document.querySelector('#item2').remove();
-
-const element=document.createElement('element');
-element.addEventListener("mouseenter", () => {
-  console.log("Mouse entered");
-});
-
-element.addEventListener("mouseleave", () => {
-  console.log("Mouse left");
-});
-
-document.addEventListener("keydown", e => {
-  console.log(e.key);
-});
-
-document.addEventListener('keydown' , e =>{
-    console.log(e.key);
-})
+// document.body.prepend(newDiv);
+// const parent = document.querySelector("#list");
+// const ref = document.querySelector("#item2");
+// const newItem = document.createElement("li");
+// newItem.textContent = "Inserted Item";
+// parent.insertBefore(newItem, ref);
 
 
-document.addEventListener('keydown' , e =>{
-    if(e.key==='enter'){
-        console.log('Enter pressed');
-    }
-})
+// // parent.removerChild(ref);
+// // document.querySelector('#item2').remove();
 
+// const element=document.createElement('element');
+// element.addEventListener("mouseenter", () => {
+//   console.log("Mouse entered");
+// });
+
+// element.addEventListener("mouseleave", () => {
+//   console.log("Mouse left");
+// });
+
+// document.addEventListener("keydown", e => {
+//   console.log(e.key);
+// });
+
+// document.addEventListener('keydown' , e =>{
+//     console.log(e.key);
+// })
+
+
+// document.addEventListener('keydown' , e =>{
+//     if(e.key==='enter'){
+//         console.log('Enter pressed');
+//     }
+// })
+ 
 const h1=document.createElement('h1');
 h1.className='text';
 h1.id='title';
+document.body.appendChild(h1);
 
 const title=document.getElementById('title');
 console.log(title);
+
+
+const p=document.createElement('p');
+p.className='item';
+p.id='p-item';
+document.body.appendChild(p);
+
+document.getElementById('p-item');
+document.getElementsByClassName('item');
+document.querySelector('#p-item');
+document.querySelectorAll('.p-item');
+console.log('hello world');
+console.log(p.parentElement);
+
+
+const box=document.createElement('box');
+box.id='box1';
+document.body.appendChild(box);
+console.log(box.children);
+
+
+
+
+
+
+
+
+
+
 
 
 
